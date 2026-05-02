1606c400bcc28c91394c82419878128aabe89235
PVMChannel <45013945+PVMChannel@users.noreply.github.com>
2026-05-02 12:55:06 +0200

    fix ffmpeg check not working on windows

--- Models/FFmpegDownloaderModel.cs
+++ Models/FFmpegDownloaderModel.cs
@@ -1,6 +1,8 @@
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 using System.Threading.Tasks;
 using FFMpegCore;
 using FFMpegCore.Exceptions;
@@ -11,14 +13,23 @@ namespace DiscordifyVideo.Models;
 
 public class FFMpegDownloaderModel
 {
+    private static bool IsFFmpegNotFoundException(Exception exception)
+    {
+        return (
+                (exception is FFMpegException) || // linux probably
+                (exception is Win32Exception winexception && winexception.NativeErrorCode == 2) // windows
+            );
+    }
     public static bool CheckAndFindFFMpeg()
     {
         try{
             FFMpeg.GetCodecs();
             return true;
         }
-        catch (FFMpegException)
+        catch (Exception exception)
         {
+            if(!IsFFmpegNotFoundException(exception)) throw;
+
             string previousBinaryFolder = GlobalFFOptions.Current.BinaryFolder;
 
             try
@@ -27,8 +38,10 @@ public class FFMpegDownloaderModel
                 FFMpeg.GetCodecs();
                 return true;
             }
-            catch
+            catch(Exception exception2)
             {
+                if(!IsFFmpegNotFoundException(exception2)) throw;
+
                 GlobalFFOptions.Current.BinaryFolder = previousBinaryFolder;
                 return false;
             }
